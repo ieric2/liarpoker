2b8991c3c8bccfcfddb9f157b7ec97659d122e5b
ieric2 <ieric@seas.upenn.edu>
2020-04-26 18:48:11 -0400

    added hand increasing invariant

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var socketList = {};
 var turnList = [];
 var playerList = {};
 var playerArray = [];
-var playerCount = 0;
 var playerTurn = null;
 var freeCards = [ "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
               "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
@@ -45,8 +44,17 @@ class Player{
   }
 }
 
+function joinGame(socket){
+  playerList[socket.id] = new Player(socket.id);
+  playerList[socket.id].setName(socket.id);
+  playerArray.push(socket.id);
+  io.emit("addToChat", socket.id + " has joined the game");
+  console.log("the number of players is: " + playerArray.length);
+  console.log("my name is: " + playerList[socket.id].name);
+}
+
 function drawCards(id){
-  for (var i = 0; i < playerCount; i++){
+  for (var i = 0; i < playerArray.length; i++){
     card = freeCards[Math.floor(Math.random() * freeCards.length)];
     playerList[id].cards.push(card);
     freeCards.splice(freeCards.indexOf(card), 1);
@@ -71,6 +79,61 @@ function checkHand(socket, data){
 
 }
 
+function checkHandIncreases(data){
+  prevHand = turnList[turnList.length - 1];
+  prevHandType = prevHand[1];
+  prevHandSubtype = prevHand[2];
+  prevHandSubtype2 = prevHand[3];
+
+  handType = data[0];
+  if (handType < prevHandType){
+    return false;
+  }
+  else if (handType > prevHandType){
+    return true;
+  }
+  else{
+    switch(prevHandType){
+      case 9: //royalFlush
+        return false;
+      case 8: //straightFlush
+        return prevHandSubtype2 < data[2];
+      case 7: //quad
+        return prevHandSubtype < data[1];
+      case 6: // fullHouse
+        return (prevHandSubtype < data[1]) || (prevHandSubtype == data[1] && prevHandSubtype2 < data[2]);
+      case 5: // flush
+        return false;
+      case 4: // straight
+        return prevHandSubtype < data[1];
+      case 3: //triple
+        return prevHandSubtype < data[1];
+      case 2: //twoPair
+        return  (Math.max(prevHandSubtype, prevHandType2) < Math.max(data[1], data[2])) || 
+                (Math.max(prevHandSubtype, prevHandType2) == Math.max(data[1], data[2]) && Math.min(prevHandSubtype, prevHandType2) < Math.min(data[1], data[2]));
+      case 1: //pair
+        return prevHandSubtype < data[1];
+      case 0: //highCard
+        return prevHandSubtype < data[1];
+    }
+  }
+}
+
+function convertCardValue(value){
+  switch (value){
+    case 'J':
+      return 'a';
+    case 'Q':
+      return 'b';
+    case 'K':
+      return 'c';
+    case 'A':
+      return 'd';
+    default:
+      return value;
+  }
+}
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
   console.log("client connection");
@@ -79,13 +142,7 @@ io.sockets.on('connection', function(socket){
   socketList[socket.id] = socket;
 
   socket.on("joinGame", function(){
-    playerCount++;
-    playerList[socket.id] = new Player(socket.id);
-    playerList[socket.id].setName(socket.id);
-    playerArray.push(socket.id);
-    io.emit("addToChat", socket.id + " has joined the game");
-    console.log("the number of players is: " + playerCount);
-    console.log("my name is: " + playerList[socket.id].name);
+    joinGame(socket);
   });
 
   socket.on("setName", function(data){
@@ -93,11 +150,22 @@ io.sockets.on('connection', function(socket){
   });
 
   socket.on('startGame', function(){
-    drawCards(socket.id);
-    playerTurn = Math.floor(Math.random() * playerCount);
-    for(i in socketList){
-      socketList[i].emit('gameStarting', {cards: playerList[i].cards});
+
+    console.log(playerList);
+
+    for (i in socketList){
+      if (playerList[i] == null){
+        joinGame(socketList[i]);
+      }
     }
+    drawCards(socket.id);
+
+    playerTurn = Math.floor(Math.random() * playerArray.length);
+
+    for(i in socketList){
+      socketList[i].emit('gameStarting', {cards: playerList[i].cards, newPlayerTurn: playerList[playerArray[playerTurn]].name});
+    }
+    console.log(playerArray);
     console.log("game Starting");  
   });
 
@@ -108,47 +176,76 @@ io.sockets.on('connection', function(socket){
     if (socket.id == playerArray[playerTurn]){
       console.log("turn played");
       console.log("hand: "+ data[0] + data[1] + data[2]);
-      switch(data[0]){
-        case "royalFlush":
-          message = playerList[socket.id].name + " played a ROYAL FLUSH"; 
-          break;
-        case "straightFlush":
-          message = playerList[socket.id].name + " played a " + data[2] + " " + data[1] + " STRAIGHT FLUSH";
-          break;
-        case "quad":
-          message = playerList[socket.id].name + " played FOUR " + data[1] + "'s";
-          break;
-        case "fullHouse":
-          message = playerList[socket.id].name + " played " + data[1] + "'s FULL of " + data[2] + "'s";
-          break;
-        case "flush":
-          message = playerList[socket.id].name + " played a " + data[1] + " FLUSH";
-          break;
-        case "straight":
-          message = playerList[socket.id].name + " played a " + data[1] + " STRAIGHT";
-          break;
-        case "triple":
-          message = playerList[socket.id].name + " played THREE " + data[1] + "'s";
-          break;
-        case "twoPair":
-          message = playerList[socket.id].name + " played a PAIR of " + data[1] + "'s and a PAIR of " + data[2] + "'s";
-          break;
-        case "pair":
-          message = playerList[socket.id].name + " played a PAIR of " + data[1] + "'s";
-          break;
-        case "highCard":        
-          message = playerList[socket.id].name + " played HIGH CARD " + data[1];
-          break;
+      if (turnList.length == 0 || checkHandIncreases(data)){
+        switch(data[0]){
+          case "royalFlush":
+            message = playerList[socket.id].name + " played a ROYAL FLUSH";
+            data[0] = 9;
+            break;
+          case "straightFlush":
+            message = playerList[socket.id].name + " played a " + data[2] + " high " + data[1] + " STRAIGHT FLUSH";
+            data[0] = 8;
+            data[2] = convertCardValue(data[2]);
+            break;
+          case "quad":
+            message = playerList[socket.id].name + " played FOUR " + data[1] + "'s";
+            data[0] = 7;
+            data[2] = convertCardValue(data[1]);
+            break;
+          case "fullHouse":
+            message = playerList[socket.id].name + " played " + data[1] + "'s FULL of " + data[2] + "'s";
+            data[0] = 6;
+            data[1] = convertCardValue(data[1]);
+            data[2] = convertCardValue(data[2]);
+            if (data[1] == data[2]){
+              console.log("can't flush same card");
+              return;
+            }
+            break;
+          case "flush":
+            message = playerList[socket.id].name + " played a " + data[1] + " FLUSH";
+            data[0] = 5;
+            break;
+          case "straight":
+            message = playerList[socket.id].name + " played a " + data[1] + " high STRAIGHT";
+            data[0] = 4;
+            data[1] = convertCardValue(data[1]);
+            break;
+          case "triple":
+            message = playerList[socket.id].name + " played THREE " + data[1] + "'s";
+            data[0] = 3;
+            data[1] = convertCardValue(data[1]);
+            break;
+          case "twoPair":
+            message = playerList[socket.id].name + " played a PAIR of " + data[1] + "'s and a PAIR of " + data[2] + "'s";
+            data[0] = 2;
+            data[1] = convertCardValue(data[1]);
+            data[2] = convertCardValue(data[2]);
+            if (data[1] == data[2]){
+              console.log("can't two pair same card");
+              return;
+            }
+            break;
+          case "pair":
+            message = playerList[socket.id].name + " played a PAIR of " + data[1] + "'s";
+            data[0] = 1;
+            data[1] = convertCardValue(data[1]);
+            break;
+          case "highCard":        
+            message = playerList[socket.id].name + " played HIGH CARD " + data[1];
+            data[0] = 0;
+            data[1] = convertCardValue(data[1]);
+            break;
+        }
+    
+        turnList.push([playerList[socket.id].name, data[0], data[1], data[2], message]);
+    
+        io.emit("addToChat", '<i>' + message + '</i>');
+    
+        playerTurn = ++playerTurn % playerArray.length;
+    
+        io.emit("updateGame", {newPlayerTurn: playerList[playerArray[playerTurn]].name, pastMove: turnList[turnList.length - 1][4]})
       }
-  
-      turnList.push([playerList[socket.id].name, data[0], data[1], data[2], message]);
-  
-      io.emit("addToChat", '<i>' + message + '</i>');
-  
-      playerTurn = ++playerTurn % playerCount;
-  
-      io.emit("updateGame", {newPlayerTurn: playerArray[playerTurn], pastMove: turnList[turnList.length - 1][4]})
-  
     }  
   });
 
@@ -161,12 +258,12 @@ io.sockets.on('connection', function(socket){
   });
 
   socket.on('chat', function(data){
-    io.emit('addToChat', socket.id + ": " + data);
+    io.emit('addToChat', playerList[socket.id].name + ": " + data);
   })
 
   socket.on('disconnect', function(){
     delete socketList[socket.id];
     delete playerList[socket.id];
-    playerCount--;
+    // playerArray.splice(playerArray.indexOf(socket.id), 1);
   });
 });
