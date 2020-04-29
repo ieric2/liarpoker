9f34bd2e8b3c121daff00d5f1389b31649cf8771
ieric2 <ieric@seas.upenn.edu>
2020-04-29 00:04:14 -0400

    removed imcomplete code

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var freeCards = [ "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "
               "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
               "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC"];
 var usedCards = [];
+var gameStarted = [];
 
 class Player{
   constructor(id){
@@ -256,7 +257,7 @@ io.sockets.on('connection', function(socket){
   });
 
   socket.on('playTurn', function(data){
-    if ()
+    // if ()
     console.log(playerTurn);
 
     console.log("turn for: " + playerList[playerArray[playerTurn]].name);
